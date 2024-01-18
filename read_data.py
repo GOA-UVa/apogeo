f0a14df19be6727c71cfbd7e7cf6a5de314401bf
Javier Gatón Herguedas <gaton@goa.uva.es>
2024-01-18 13:40:52 +0100

    able to read and store as csv the new values

--- read_data.py
+++ read_data.py
@@ -1,29 +1,50 @@
 import time
+import os
+from io import StringIO
 
 import serial
+import pandas as pd
 
 
 class CR300():
-    def __init__(self, port: str, baudrate: int = 9600):
+    def __init__(self, port: str, baudrate: int = 9600, timeout: float = None):
         self.port = port
         self.baudrate = baudrate
-        self.s = serial.Serial(port, baudrate)
+        self.timeout = timeout
+        self.s = serial.Serial(port, baudrate, timeout=timeout)
 
     def new_serial(self):
         self.s.close()
-        self.s = serial.Serial(self.port, self.baudrate)
+        self.s = serial.Serial(self.port, self.baudrate, timeout=self.timeout)
 
     def refresh(self):
         self.s.close()
         self.s.open()
 
-    def send_command(self, cmd: str, delay=1) -> str:
-        print(f"{cmd}\r\n".encode())
-        self.s.write(f"{cmd}\r\n".encode())
+    def _send_command(self, cmd: str, delay=1) -> str:
+        cmd_send = f"{cmd}\r\n".encode()
+        self.s.write(cmd_send)
         time.sleep(delay)
         out = self.s.read_all()
-        print(out)
+        if not out.startswith(cmd_send):
+            raise Exception("DataLogger didn't answer with the sent CMD at the begining. Something wrong happened.")
         return '\n'.join(out.decode().split('\r\n'))
+
+    def send_command(self, cmd: str, delay=1, _try=0) -> str:
+        try:
+            out = self._send_command(cmd, delay)
+        except Exception:
+            if _try > 2:
+                raise Exception("Connection lost, and couldn't recover it.")
+            elif _try < 2:
+                self.refresh()
+            else:
+                self.new_serial()
+            connected = False
+            while not connected:
+                connected = self.connect()
+            out = self.send_command(cmd, delay, _try + 1)
+        return out
 
     def connect(self, delay = 0.1) -> bool:
         self.s.write(f"\r\n".encode())
@@ -34,23 +55,46 @@ class CR300():
     def close(self):
         self.s.close()
 
+    def get_one_records_data(self):
+        out_lines = self.send_command("SHOW RECORDS").splitlines()[1:-1]
+        out_lines[0] = ' '.join(['date', 'time', 'type', 'id'] + out_lines[0].split()[1:])
+        out = '\n'.join(out_lines).replace('   ', ' ')
+        df = pd.read_csv(StringIO(out), sep=' ', header=0, index_col=3)
+        df['datetime'] = pd.to_datetime(df['date'] + df['time'], format='%Y/%m/%d%H:%M:%S.%f')
+        df.drop(['date', 'time', 'type'], inplace=True, axis=1)
+        return df
+
+    def get_last_records_data(self):
+        dfs = []
+        keep_seeking = True
+        while keep_seeking:
+            df = self.get_one_records_data()
+            dfs.append(df)
+            if len(df) < 20:
+                keep_seeking = False
+        df = pd.concat(dfs)
+        return df
 
 def main():
     #port = '/dev/serial/by-id/usb-Campbell_Scientific__Inc._CR300_00000000050C-if00'
     port = '/dev/ttyACM0'
+    outpath = 'out/data.csv'
     cr300 = CR300(port)
     connected = False
     print('Connecting...')
     while not connected:
         connected = cr300.connect()
     print('Connected')
-    resp = cr300.send_command('SHOW RECORDS')
-    print(resp)
-
-    resp = cr300.send_command('HELP')
-    print(resp)
+    df = cr300.get_last_records_data()
     cr300.close()
-    
+    if os.path.exists(outpath):
+        df0 = pd.read_csv(outpath, index_col=0)
+        if df.empty:
+            df = df0
+        else:
+            df = pd.concat([df0, df])
+    df.to_csv(outpath)
+
 
 if __name__ == '__main__':
     main()
