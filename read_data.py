baa3d337dd641b6f19aa95fa77727943cd725133
Javier Gatón Herguedas <gaton@goa.uva.es>
2024-01-22 10:28:16 +0100

    data is correctly read

--- read_data.py
+++ read_data.py
@@ -91,15 +91,18 @@ def main():
     while not connected:
         connected = cr300.connect()
     print('Connected')
-    df = cr300.get_all_records_data()
-    cr300.close()
-    if os.path.exists(outpath):
-        df0 = pd.read_csv(outpath, index_col=0)
-        if df.empty:
-            df = df0
-        else:
-            df = pd.concat([df0, df])
-    df.to_csv(outpath)
+    #df = cr300.get_all_records_data()
+    #cr300.close()
+    while True:
+        df = cr300.get_last_records_data()
+        if os.path.exists(outpath):
+            df0 = pd.read_csv(outpath, index_col=0)
+            if df.empty:
+                df = df0
+            else:
+                df = pd.concat([df0, df])
+        df.to_csv(outpath)
+        time.sleep(60)
 
 
 if __name__ == '__main__':
