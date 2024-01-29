d44f6f68f377824956f938bc5ca4fad0735d621b
Javier Gatón Herguedas <gaton@goa.uva.es>
2024-01-29 14:38:56 +0100

    updated with windows port and new file each day

--- read_data.py
+++ read_data.py
@@ -1,5 +1,6 @@
 import time
 import os
+from datetime import datetime, timezone as tz 
 
 import pandas as pd
 
@@ -7,11 +8,15 @@ from cr300.cr300 import CR300
 
 
 def main():
-    port = '/dev/serial/by-id/usb-Campbell_Scientific__Inc._CR300_00000000050C-if00'
+    port = 'COM4'
+    # In Linux it would be:
+    #port = '/dev/serial/by-id/usb-Campbell_Scientific__Inc._CR300_00000000050C-if00'
     #port = '/dev/ttyACM0'
-    outpath = 'out/data.csv'
     cr300 = CR300(port)
     connected = False
+    out_dir = 'out'
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
     print('Connecting...')
     while not connected:
         connected = cr300.connect()
@@ -19,6 +24,8 @@ def main():
     #df = cr300.get_all_records_data()
     #cr300.close()
     while True:
+        dt = datetime.now(tz=tz.utc)
+        outpath = os.path.join(out_dir, dt.strftime('%Y-%m-%d.csv'))
         df = cr300.get_last_records_data()
         if os.path.exists(outpath):
             df0 = pd.read_csv(outpath, index_col=0)
